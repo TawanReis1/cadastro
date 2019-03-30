87fd97d23d18a228fb18545cbc225227d0286b23
Tawan Reis <tawan.reis@linkapi.com.br>
2019-03-30 17:09:43 -0300

    Criação BD e estruturação classe cliente

--- src/java/br/com/fatecpg/cadastro/Cliente.java
+++ src/java/br/com/fatecpg/cadastro/Cliente.java
@@ -10,5 +10,54 @@ package br.com.fatecpg.cadastro;
  * @author User
  */
 public class Cliente {
-    
+
+    String nome, cpf, rg, email, telefone, endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
